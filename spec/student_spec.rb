44c049a670cf7f6fc6a02006e90209cedd71c2d3
jmonto55 <jmonto55@gmail.com>
2023-04-20 15:32:12 -0500

    Add spec test file for Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../student'
 
 describe Student do
-  context '' do
-    it '' do
+  context 'Should create a new instance of Student class' do
+    it 'The initialize method returns a Student object with the attributes provided' do
       age = 19
       name = 'Bob Lazar'
       parent_permission = false
@@ -13,15 +13,14 @@ describe Student do
     end
   end
 
-  context '' do
-    it '' do
+  context 'Should instance the method play_hocky from Student class' do
+    it 'The play_hocky method returns "¯\(ツ)/¯"' do
       age = 19
       name = 'Bob Lazar'
       parent_permission = false
 
       student = Student.new(age, name, parent_permission)
-      expect(student.name).to eq name
-      expect(student.age).to eq age
+      expect(student.play_hooky).to eq '¯\(ツ)/¯'
     end
   end
 end
