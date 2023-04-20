c18b7142f81cb2fb1e4e2e83e95204aac0ade4f4
jmonto55 <jmonto55@gmail.com>
2023-04-20 17:02:23 -0500

    Fix rubocop errors

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../classroom'
 require_relative '../student'
 describe Classroom do
-    context 'when testing the Classroom class' do
-        label = 'Maths'
-        student = Student.new(25,'Tom',parent_permission:true)
-        classroom = Classroom.new(label)
-        
-     it 'check for the label' do
-        expect(classroom.label).to eq label
-        end
-        
-        it 'validate length of the array students' do
-            classroom.add_student(student)
-            expect(classroom.students.length).to eql 1
-            end
+  context 'when testing the Classroom class' do
+    label = 'Maths'
+    student = Student.new(25, 'Tom', parent_permission: true)
+    classroom = Classroom.new(label)
+
+    it 'check for the label' do
+      expect(classroom.label).to eq label
     end
+
+    it 'validate length of the array students' do
+      classroom.add_student(student)
+      expect(classroom.students.length).to eql 1
+    end
+  end
 end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,24 +1,24 @@
-require_relative '../teacher.rb'
+require_relative '../teacher'
 
 describe Teacher do
-    context 'When testing the teacher class' do
-        name = 'balix'
-        age = '25'
-        specialization = 'Maths'
-        teacher = Teacher.new(age,name,specialization)
+  context 'When testing the teacher class' do
+    name = 'balix'
+    age = '25'
+    specialization = 'Maths'
+    teacher = Teacher.new(age, name, specialization)
 
-        it 'validate the age of teacher' do
-            expect(teacher.age).to eq age
-        end
-        it 'validate the name of teacher' do
-            expect(teacher.name).to eq name
-        end
+    it 'validate the age of teacher' do
+      expect(teacher.age).to eq age
     end
+    it 'validate the name of teacher' do
+      expect(teacher.name).to eq name
+    end
+  end
 
-    context 'for the can_use_services? methode' do
-        it 'can_use_services? method' do
-            teacher = Teacher.new(25,"paul","sst")
-            expect(teacher.can_use_services?).to eql true
-        end
+  context 'for the can_use_services? methode' do
+    it 'can_use_services? method' do
+      teacher = Teacher.new(25, 'paul', 'sst')
+      expect(teacher.can_use_services?).to eql true
     end
+  end
 end
