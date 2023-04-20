cbd8b27b07461594cc3e3c7d9c31cad16c5df06d
jmonto55 <jmonto55@gmail.com>
2023-04-20 10:15:41 -0500

    refactor to make code dry

--- app.rb
+++ app.rb
@@ -57,7 +57,6 @@ class App
 
   def load_rentals
     return [] unless File.exist?('data/rentals.json')
-
     JSON.parse(File.read('data/rentals.json')).map do |rental|
       book = Book.new(rental['book']['title'], rental['book']['author'])
       if rental['person']['class'] == 'Teacher'
@@ -71,7 +70,13 @@ class App
     end
   end
 
-  def load_books; end
+  def load_books
+    return [] unless File.exist?('data/books.json')
+
+    JSON.parse(File.read('data/books.json')).map do |book|
+      Book.new(book['title'], book['author'])
+    end
+  end
 
   def load_persons
     return [] unless File.exist?('data/persons.json')
